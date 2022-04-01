003a0428ea5eea280b02d6691853f0e2e3a106dc
salikur6 <salikur6@gmail.com>
2022-04-01 09:07:50 +0600

    doing some changes

--- src/components/Contact/BdAddress.js
+++ src/components/Contact/BdAddress.js
@@ -5,10 +5,10 @@ const BdAddress = () => {
     <div className='border p-8 bg-blue-100 w-full'>
       <div className='flex justify-between items-center'>
         <div>
-          <h1>Street: 17/4 Dhanmondi.</h1>
-          <h1>City: Dhaka</h1>
-          <h1>Phone: +8801622222456</h1>
-          <h1>Email: contact.bd@cc.com</h1>
+          <h1>Street: Alompur, 27No Ward.</h1>
+          <h1>City: Sylhet</h1>
+          <h1>Phone: +880***********</h1>
+          <h1>Email: salikur6@gmailcom</h1>
         </div>
         <div>
           <h1 className='text-2xl'>BANGLADESH OFFICE</h1>
